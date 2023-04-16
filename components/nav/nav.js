9b03875fb98f328dc1881fadee6c46f51fd140e7
Regan-Koopmans <regankoop20@gmail.com>
2023-04-16 14:37:12 +0200

    Update site

--- components/nav/nav.js
+++ components/nav/nav.js
@@ -2,9 +2,8 @@ class NavBar extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
         <div class="links">
-            <a href="index.html">Home</a>
-            <a href="blog.html">Blog</a>
-            <a href="experiments.html">Experiments</a>
+            <a href="/index.html">Home</a>
+            <a href="/blog.html">Blog</a>
             <a href="https://github.com/Regan-Koopmans" target="_blank">GitHub</a>
         </div>
     `;
