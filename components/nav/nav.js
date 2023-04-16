70a2018aabae1cd17e707a298bb96cae2bc59b54
Regan-Koopmans <regankoop20@gmail.com>
2023-04-16 14:04:22 +0200

    Update site

---
+++ components/nav/nav.js
@@ -0,0 +1,14 @@
+class NavBar extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <div class="links">
+            <a href="index.html">Home</a>
+            <a href="blog.html">Blog</a>
+            <a href="experiments.html">Experiments</a>
+            <a href="https://github.com/Regan-Koopmans" target="_blank">GitHub</a>
+        </div>
+    `;
+    }
+}
+
+customElements.define('site-nav', NavBar);
