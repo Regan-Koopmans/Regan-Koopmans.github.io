3dd8099fede1a7188627281731e95ae96949fe6a
Regan Koopmans <regan@planet.com>
2024-01-15 16:18:04 +0100

    Add to blog

--- load.js
+++ load.js
@@ -1,14 +1,14 @@
-if (typeof InstantClick === 'undefined') {
-    const scriptPromise = new Promise((resolve, reject) => {
-        const script = document.createElement('script');
-        document.body.appendChild(script);
-        script.onload = resolve;
-        script.onerror = reject;
-        script.async = true;
-        script.src = '/instantclick.min.js';
-    });
-
-    scriptPromise.then(() => {
-        InstantClick.init()
-    });
-}
+// if (typeof InstantClick === 'undefined') {
+//     const scriptPromise = new Promise((resolve, reject) => {
+//         const script = document.createElement('script');
+//         document.body.appendChild(script);
+//         script.onload = resolve;
+//         script.onerror = reject;
+//         script.async = true;
+//         script.src = '/instantclick.min.js';
+//     });
+//
+//     scriptPromise.then(() => {
+//         InstantClick.init()
+//     });
+// }
